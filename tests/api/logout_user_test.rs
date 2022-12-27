12192b2cde8fed887e428d5986827ee06ef88ba6
sani haq <work.sanihaq@gmail.com>
2022-12-28 00:35:06 +0530

    logout test

---
+++ tests/api/logout_user_test.rs
@@ -0,0 +1,75 @@
+use crate::helpers::TEST_USER;
+
+use super::helpers::{drop_database_after_test, spawn_app};
+use sea_orm::Set;
+use todo_server_axum::database::users::{self};
+use todo_server_axum::queries::user_queries::{find_by_username, save_active_user};
+use todo_server_axum::utilities::hash::hash_password;
+use todo_server_axum::utilities::jwt::create_token;
+
+#[tokio::test]
+async fn logout_user_works() {
+    let (state, db_info) = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let mut user = users::ActiveModel {
+        ..Default::default()
+    };
+
+    user.username = Set(TEST_USER.username.into_owned());
+    user.password =
+        Set(hash_password(&TEST_USER.password.into_owned()).expect("error hashing password."));
+
+    let token = create_token(&state.jwt_secret.0, TEST_USER.username.into_owned())
+        .expect("error creating token.");
+
+    user.token = Set(Some(token.clone()));
+
+    let _ = save_active_user(&state.db, user).await.expect(
+        format!(
+            "Unable to save in database.  port: {}, db: {}",
+            state.port, db_info.name
+        )
+        .as_str(),
+    );
+
+    let response = client
+        .post(&format!("{}:{}/api/v1/users/logout", state.uri, state.port))
+        .header("x-auth-token", token)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(
+        response.status().is_success(),
+        "status code was: {}, expected code was 200. port: {}, db: {}",
+        response.status(),
+        state.port,
+        db_info.name
+    );
+
+    let user = find_by_username(&state.db, TEST_USER.username.into_owned())
+        .await
+        .expect("Couldn't found user in database");
+
+    assert_eq!(
+        user.id, 1,
+        "user-id should not be {}. port: {}, db: {}",
+        user.id, state.port, db_info.name
+    );
+
+    match user.token {
+        Some(token) => assert_eq!(
+            token, "",
+            "token should be empty, got {}. port: {}, db: {}",
+            token, state.port, db_info.name
+        ),
+        None => assert!(
+            true,
+            "token not found in database: {}, port: {}",
+            db_info.name, state.port
+        ),
+    };
+
+    drop_database_after_test(state.db, db_info).await;
+}
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -3,3 +3,4 @@ mod create_user_test;
 mod health_check_test;
 mod helpers;
 mod login_user_test;
+mod logout_user_test;

3e46cab10c968dad659bc61b27c303b4f1756047
sani haq <work.sanihaq@gmail.com>
2022-12-28 18:55:33 +0530

    more robust test for create user

--- tests/api/create_user_test.rs
+++ tests/api/create_user_test.rs
@@ -1,24 +1,24 @@
 use crate::helpers::TEST_USER;
 
 use super::helpers::{drop_database_after_test, spawn_app};
-use reqwest::StatusCode;
 use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use todo_server_axum::database::users::{self, Entity as Users};
 use todo_server_axum::routes::users::{RequestCreateUser, ResponseUser};
+use todo_server_axum::utilities::hash::verify_password;
 
 #[tokio::test]
 async fn create_user_works() {
     let (state, db_info) = spawn_app().await;
     let client = reqwest::Client::new();
 
-    let user = RequestCreateUser {
+    let req_user = RequestCreateUser {
         username: TEST_USER.username.into_owned(),
         password: TEST_USER.password.into_owned(),
     };
 
     let response = client
         .post(&format!("{}:{}/api/v1/users", state.uri, state.port))
-        .json(&user)
+        .json(&req_user)
         .send()
         .await
         .expect("Failed to execute request.");
@@ -32,62 +32,72 @@ async fn create_user_works() {
     );
 
     match Users::find()
-        .filter(users::Column::Username.eq(user.username.clone()))
+        .filter(users::Column::Username.eq(req_user.username.clone()))
         .one(&state.db)
         .await
     {
         Ok(u) => match u {
-            Some(user) => assert_eq!(
-                user.id, 1,
-                "On database id shouldn't be {}. port: {}, db: {}",
-                user.id, state.port, db_info.name
-            ),
+            Some(user) => {
+                // check against database and request
+                assert_eq!(
+                    user.id, 1,
+                    "On database id shouldn't be {}, expected 1. port: {}, db: {}",
+                    user.id, state.port, db_info.name
+                );
+                assert_eq!(
+                    user.username, req_user.username,
+                    "On database username should be {}, but it's {}. port: {}, db: {}",
+                    req_user.username, user.username, state.port, db_info.name
+                );
+                let is_verified = verify_password(&req_user.password, &user.password)
+                    .expect("error hashing password.");
+                assert_eq!(
+                    is_verified, true,
+                    "Verification failed on password from database. port: {}, db: {}",
+                    state.port, db_info.name
+                );
+                match response.json::<ResponseUser>().await {
+                    // check against Response and database
+                    Ok(data) => {
+                        assert_eq!(
+                            data.id, 1,
+                            "Received id {} as response, should be 1. port: {}, db: {}",
+                            data.id, state.port, db_info.name
+                        );
+                        assert_eq!(
+                            data.username, req_user.username,
+                            "Received username {} as response, expected {}. port: {}, db: {}",
+                            data.username, user.username, state.port, db_info.name
+                        );
+                        match user.token {
+                            Some(token) => assert_eq!(
+                                data.token, token,
+                                "token should be the same. on database {}, received {}. port: {}, db: {}",
+                                data.username, token, state.port, db_info.name
+                            ),
+                            None => panic!(
+                                "token not found in database: {}, port: {}",
+                                db_info.name, state.port
+                            ),
+                        };
+                    }
+                    Err(e) => {
+                        panic!("Error decoding response: , {}", e);
+                    }
+                }
+            }
             None => panic!(
                 "username with {}, not found in database: {}, port: {}",
-                user.username, db_info.name, state.port
+                req_user.username, db_info.name, state.port
             ),
         },
         Err(e) => {
-            eprintln!(
+            panic!(
                 "Problem finding user on db. DbErr occurred , {}. port: {}, db: {}",
                 e, state.port, db_info.name
             );
         }
     }
-
-    match response.json::<ResponseUser>().await {
-        Ok(data) => {
-            assert_eq!(
-                data.id, 1,
-                "user-id should not be {}. port: {}, db: {}",
-                data.id, state.port, db_info.name
-            );
-            assert_eq!(
-                data.username, user.username,
-                "expected username was {}, got {}. port: {}, db: {}",
-                data.username, user.username, state.port, db_info.name
-            );
-        }
-        Err(e) => {
-            eprintln!("Error decoding response: , {}", e);
-        }
-    }
-
-    let response = client
-        .post(&format!("{}:{}/api/v1/users", state.uri, state.port))
-        .json(&user)
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    assert_eq!(
-        response.status(),
-        StatusCode::CONFLICT,
-        "expected status code was 409(CONFLICT), got {}. port: {}, db: {}",
-        response.status(),
-        state.port,
-        db_info.name
-    );
 
     drop_database_after_test(state.db, db_info).await;
 }
